9dfc2071e7e8531710c250ec658518c542ce6b44
Marcin Chruszcz <chinek01@o2.pl>
2023-07-31 14:40:20 +0200

    game core class

--- game_core/game_core.py
+++ game_core/game_core.py
@@ -10,7 +10,50 @@ Game Core class
 
 """
 
+from turtle import textinput
+
+
+LIFE = 3
+
 
 class Game_core:
 
-    pass
+    def __init__(self):
+        self.cur_life = LIFE
+        self.is_game_over_flag = True
+        self.is_start_game_flag = False
+        self.name = 'MC'
+
+    def set_win_name(self):
+        self.name = textinput(
+            "Enter your name: ",
+            "Name: "
+        )
+
+    def loose_life(self):
+        self.cur_life -= 1
+        self.check_life()
+
+    def check_life(self):
+        if self.cur_life == 0:
+            self.is_game_over_flag = False
+
+    def get_game_over_flag(self):
+        return self.is_game_over_flag
+
+    def start_game(self):
+        if self.is_start_game_flag is False:
+            self.is_start_game_flag = True
+            self.is_game_over_flag = False
+
+    def reset_game(self):
+        self.cur_life = LIFE
+        self.is_game_over_flag = True
+        self.is_start_game_flag = False
+
+
+# some test
+if __name__ == '__main__':
+    x = Game_core()
+    x.set_win_name()
+    print(f"{x.name}")
